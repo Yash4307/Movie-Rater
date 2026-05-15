5de122f0c78e4ba550a87b31e2adee9d8a5266b0
Yash4307 <157444526+Yash4307@users.noreply.github.com.>
2026-05-16 00:26:04 +0530

    Admin addition temporary code removal

--- app.py
+++ app.py
@@ -498,19 +498,5 @@ def get_trending_movies():
 with app.app_context():
     db.create_all()
 
-    # temporary admin user creation for testing
-    admin = User.query.filter_by(username='admin').first()
-
-    if not admin:
-        admin_user = User(
-            username='admin',
-            email='admin@gmail.com',
-            password=hash_password('admin123'),
-            is_admin=True
-        )
-
-        db.session.add(admin_user)
-        db.session.commit()
-
 if __name__ == '__main__':
     app.run(debug=True)
